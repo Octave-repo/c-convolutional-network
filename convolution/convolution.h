33c16f508ad11c81bf9053195592911a5821dd59
Octave-portfolio <valadieroctave@gmail.com>
2021-01-18 13:12:30 +0100

    Incroyable first time de la conv2d

--- convolution/convolution.h
+++ convolution/convolution.h
@@ -5,5 +5,5 @@
 int conv1d(Matrice in_feature_map, Matrice kernel,
  unsigned short stride, unsigned short padding, Matrice *out_feature_ma);
 
-int conv2d(Matrice *in_feature_map, Matrice *kernel,
- unsigned short stride, unsigned short padding, Matrice *in_feature_ma);
+int conv2d(Matrice in_feature_map, Matrice kernel,
+ unsigned short stride, unsigned short padding, Matrice *out_feature_map);
